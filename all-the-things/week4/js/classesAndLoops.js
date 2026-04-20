7389d3bf7bcc30d96e150f76db5f29483afbf3b8
fard442 <mrlutton@email.neit.edu>
2026-04-20 09:52:27 -0400

    update

--- all-the-things/week4/js/classesAndLoops.js
+++ all-the-things/week4/js/classesAndLoops.js
@@ -4,23 +4,30 @@ var pen = canvas.getContext("2d");
 var interval = 1000/60;
 setInterval(game, interval);
 
-function createBall(){
-    var ball = {
+function createGameObject(){
+    var gameObject = {
         x:randomNumber(15, canvas.width - 15),
         y:randomNumber(15, canvas.height - 15),
         moveX:setRandomNumber(),
         moveY:setRandomNumber(),
         color:`rgb(${randomNumber(0,255)}, ${randomNumber(0,255)}, ${randomNumber(0,255)})`,
         radius:15,
+        width:15,
+        height:15,
         drawBall:function()
         {
             pen.beginPath();
             pen.fillStyle = this.color;
             pen.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
             pen.fill();
+        },
+        drawSquare:function()
+        {
+            pen.fillStyle = this.color;
+            pen.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    return ball
+    return gameObject;
 }
 
 function randomNumber(low,high){
@@ -36,17 +43,25 @@ function setRandomNumber(){
     }
 }
 
-var myBall = createBall();
+var myBall = createGameObject();
 var myBalls = [];
 
+var player = createGameObject();
+player.x = canvas.width/2;
+player.y = canvas.height/2;
+player.width = 30;
+player.height = 30;
+player.color = "purple";
+
 for(var i=0; i<10; i++){
-    myBalls[i] = createBall();
+    myBalls[i] = createGameObject();
 }
 
 function game(){
     pen.clearRect(0, 0, canvas.width, canvas.height);
 
     //myBall.drawBall();
+    player.drawSquare();
 
     for(var i=0; i<myBalls.length; i++){
         myBalls[i].drawBall();
